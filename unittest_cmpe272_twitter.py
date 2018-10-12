ef9b4a40e66f2bd5408a4e2350494406fb2584ed
Tracy Ma <tracy.2533@gmail.com>
2018-10-11 22:06:45 -0700

    added unittest for backend APIs

---
+++ unittest_cmpe272_twitter.py
@@ -0,0 +1,32 @@
+import datetime
+import unittest
+import re
+import json
+import flask
+import cmpe272_twitter as server
+
+
+class Test_Flask_App(unittest.TestCase):
+    def setUp(self):
+        server.app.config['TESTING'] = True
+
+    def test_index(self):
+        with server.app.test_client() as client:
+            resp = client.get('/')
+            self.assertTrue('Yet Another Twitter API playground' in resp.data.decode('utf-8'))
+
+    def test_timeline(self):
+        with server.app.test_client() as client:
+            resp = client.post('/timeline',
+                    data={'screenname': "twitterapi", 'count': 4})
+
+            self.assertTrue(len(resp.data.decode('utf-8')) > 0 )
+
+    def test_poststatus(self):
+        with server.app.test_client() as client:
+            resp = client.post('/poststatus',
+                    data={'text': "Hello" + str(datetime.datetime.now())})
+            self.assertTrue('{"status":"ok"}', resp.data.decode('utf-8'))
+
+if __name__ == '__main__':
+    unittest.main()                    
---
+++ unittest_cmpe272_twitter_api.py
@@ -0,0 +1,32 @@
+import datetime
+import unittest
+from cmpe272_twitter_api import Cmpe272_Twitter_API as twitter
+import api_key as t
+
+class Test_Twitter_API(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_Init(self):
+        api = twitter(t.CONSUMER_KEY, t.CONSUMER_SECRET, t.ACCESS_KEY, t.ACCESS_SECRET)
+        self.assertEqual(api._timeout, 30)
+        self.assertIsNotNone(api._auth)
+
+
+    def test_GetUserTimeline(self):
+        api = twitter(t.CONSUMER_KEY, t.CONSUMER_SECRET, t.ACCESS_KEY, t.ACCESS_SECRET)
+        timeline = api.GetUserTimeline(screen_name='twitterapi', count=2)
+        self.assertIsNotNone(timeline)
+        self.assertEqual(len(timeline), 2)
+        self.assertTrue('created_at' in timeline[0])
+        self.assertTrue('text' in timeline[0])
+        self.assertTrue(len(timeline[0]['text']) > 0)
+
+    def test_PostUpdate(self):
+        api = twitter(t.CONSUMER_KEY, t.CONSUMER_SECRET, t.ACCESS_KEY, t.ACCESS_SECRET)
+        status = api.PostUpdate(status="Hello" + str(datetime.datetime.now()))
+        self.assertIsNotNone(status)
+        self.assertTrue('created_at' in status)
+
+if __name__ == '__main__':
+    unittest.main()        
